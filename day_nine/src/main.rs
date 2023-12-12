61f8a8cd6b999869ccf57a7cd9514b6defb38eac
Marc Lauder <marc@luno.com>
2023-12-12 23:47:57 +0200

    broken day nine

--- day_nine/src/main.rs
+++ day_nine/src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     part1()
 }
 
+//1842168671 - correct
+//1842168680 - incorrect
 
 fn part1() {
-    let file: File = File::open("./src/test.txt").unwrap();
+    let file: File = File::open("./src/input.txt").unwrap();
     let reader: BufReader<File> = BufReader::new(file);
 
     let mut reports: Vec<Vec<i64>> = Vec::new();
@@ -16,18 +18,47 @@ fn part1() {
         reports.push(report);
     }
 
-    let mut values: Vec<i64> = Vec::new();
+    let mut total: i64 = 0;
     for r in reports {
-        let val: i64 = extrapolate_value(&r);
+        let mut set: Vec<Vec<i64>> = vec![r];
+        set = build_layers(&set);
+        total += calc_value(&set);
     }
-    
-    let total: i64 = values.into_iter().sum();
     println!("{}", total);
 }
 
-//0 3 6 9 12 15
+fn get_deltas(row: Vec<i64>) -> Vec<i64> {
+    let mut deltas: Vec<i64> = Vec::new();
+    for i in 1..row.len() {
+        deltas.push(row[i] - row[i-1]);
+    }
+    deltas
+}
 
-fn extrapolate_value(report: &Vec<i64>) -> i64 {
-    let total: i64 = 0;
-    total
+fn build_layers(layers: &Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+    let mut new_layers = layers.clone();
+    let last = new_layers.last().unwrap();
+    if last.iter().sum::<i64>() != 0 {
+        let row = get_deltas(last.to_vec());
+        new_layers.push(row);
+        new_layers = build_layers(&new_layers).to_vec();
+    }
+    new_layers.to_vec()
+}
+
+fn calc_value(set: &Vec<Vec<i64>>) -> i64 {
+    let mut current = set.clone();
+    for i in (0..set.len()).rev() {
+        if i == set.len() - 1 {
+            current[i].push(0);
+            continue;
+        }
+        let a = *current[i].last().unwrap();
+        let b = *current[i + 1].last().unwrap();
+        current[i].push(a + b);
+    }
+    for row in &current {
+       println!("{:?}", row);
+    }
+    *current[0].last().unwrap()
 }
